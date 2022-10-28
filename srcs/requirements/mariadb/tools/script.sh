4a27fe9f727e166e328cabca3b0a313e2e5e84e1
kika <kika75@qatar75020@gmail.com>
2022-10-28 20:01:43 +0200

    improvemed on mariadb

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -3,22 +3,21 @@
 #STARTING THE MYSQL SERVICE
 service mysql start
 
-mysql_install_db
+mysql_install_db --user=mysql --datadir=/var/lib/mysql
+chown mysql:mysql /run/mysqld
 
+# THE '%' ALLOW REMOTE CONNECTION TO ALL HOST, AND LOCALHOST ONLY FROM THE DDB MACHINE
 
-# THE MYSQL INSTALLATION SCRIPT REQUIRE A PROMPT, AND IS JUST A REDIRECTION OF ANOTHER BASH SCRIPT
-# SO WE WILL SETUP THE DATA BASE MANUALLY IN PRETTY MUCH THE SAME WAY OF THE SCRIPT
+# We delete the 'anonymous user' so you cant connect with no username
+mysql -e "DELETE FROM mysql.user WHERE User=''"
 
-# THE '%' ALLOW REMOTE CONNECTION WHILE LOCALHOST DOESN'T
-
-mysql -e "DELETE FORM mysql.user WHERE User=''"
-
-mysql -e "CREATE DATABASE $MYSQL_DDB_NAME;"
+mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DDB_NAME;"
 
 mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 
-mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DDB_NAME TO '$MYSQL_USER'@'%';"
+mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DDB_NAME.* TO '$MYSQL_USER'@'%';"
+
+# We apply our changement before changing root password, otherwise the script wont be able to exec the remaining commands
+mysql -e "FLUSH PRIVILEGES;"
 
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-
-mysql -e "FLUSH PRIVILEGES;"
